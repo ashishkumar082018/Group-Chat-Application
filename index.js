bdd59b1517bab1cbf14777237f302aaf553b5c5d
ashishkumar082018 <ashishkumar082018@gmail.com>
2024-04-13 16:06:19 +0530

    Task 6 - Express Controllers

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
-
 const loginRoutes = require("./routes/login");
 const messageRoutes = require("./routes/message");
 const contactRoutes = require("./routes/contact");
+const errorController = require("./controllers/errorController");
 const rootDir = require('./util/path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
-
 app.use(loginRoutes);
 app.use(messageRoutes);
 app.use(contactRoutes);
 
-app.use((req, res) => {
-    res.sendFile(path.join(rootDir, 'views', '404.html'));
-})
+app.use(errorController.error404);
 
 const PORT = 3000;
 app.listen(PORT, () => {
