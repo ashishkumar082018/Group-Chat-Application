709c81e932e56d1765925bbd29e8cced3fd00b54
ashishkumar082018 <ashishkumar082018@gmail.com>
2024-04-13 17:23:02 +0530

    Task 6 -Express - Adding Controllers

--- routes/message.js
+++ routes/message.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const messageController = require("../controllers/messageController");
 const router = express.Router();
-
 router.use(express.json());
 
 router.get('/', messageController.getMessages);
+
+router.get('/messages', messageController.allMessages);
+
 router.post('/', messageController.postMessages);
 
 module.exports = router;
