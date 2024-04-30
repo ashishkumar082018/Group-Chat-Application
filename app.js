4df9e85b69e1bef800a84627c807a2c857464ece
Ashish kumar <ashishkumar082018@gmail.com>
2024-04-30 12:11:53 +0530

    1. Adding Signup Frontend

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
 const path = require('path');
+const cors = require('cors');
+
+
 const loginRoutes = require("./routes/login");
 const messageRoutes = require("./routes/message");
 const contactRoutes = require("./routes/contact");
@@ -8,6 +12,8 @@ const errorController = require("./controllers/errorController");
 const rootDir = require('./util/path');
 
 const app = express();
+dotenv.config();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
 app.use(loginRoutes);
