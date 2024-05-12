ebfd239fc38a610357d9975ed6845f771efef3ba
Ashish kumar <ashishkumar082018@gmail.com>
2024-05-12 16:15:28 +0530

    Prepare for Deployment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 dotenv.config();
 app.use(cors(
-    { origin: ["http://localhost:3000", 'http://127.0.0.1:5500'] }
+    { origin: ["https://group-chat-app.ashishkumar.store"] }
 ));
 
 const views = require("./routes/viewRoute");
